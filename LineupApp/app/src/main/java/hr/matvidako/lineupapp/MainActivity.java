133a63eb337dff835c6b34b5b59dab810e189e9d
Matej Vidakovic <matej.vidakovic@fiveminutes.eu>
2017-08-06 12:01:24 +0200

    logo, random cost etc

--- LineupApp/app/src/main/java/hr/matvidako/lineupapp/MainActivity.java
+++ LineupApp/app/src/main/java/hr/matvidako/lineupapp/MainActivity.java
@@ -1,12 +1,15 @@
 package hr.matvidako.lineupapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import java.util.Arrays;
 import java.util.List;
 
+import hr.matvidako.lineupapp.genre.GenresActivity;
 import hr.matvidako.lineupapp.napi.ArtistsResponse;
 import hr.matvidako.lineupapp.napi.Napi;
 import hr.matvidako.lineupapp.napi.TagsResponse;
@@ -21,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.ready).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, GenresActivity.class));
+            }
+        });
+
+
+
         napi = LineupApp.getInstance().getNapi();
         napi.getALlTags(LineupApp.CATALOG).subscribe(new Subscriber<TagsResponse>() {
             @Override
--- LineupApp/app/src/main/java/hr/matvidako/lineupapp/napi/ArtistsResponse.java
+++ LineupApp/app/src/main/java/hr/matvidako/lineupapp/napi/ArtistsResponse.java
@@ -1,6 +1,7 @@
 package hr.matvidako.lineupapp.napi;
 
 import java.util.List;
+import java.util.Random;
 
 /*
 ArtistMetadata is a simple pojo for receiving JSON responses to the getArtistMetadata napi call.
@@ -19,6 +20,7 @@ public class ArtistsResponse {
     public static class Artist {
         public String id;
         public String name;
+        public int cost;
 
         public Links links;
 
@@ -36,6 +38,34 @@ public class ArtistsResponse {
             public Link stations;
             public Link influences;
             public Link relatedProjects;
+        }
+
+        public void setCost(int chartPosition) {
+            cost = 1000000 / chartPosition;
+        }
+
+        public void setCostForSimilarArtist(int otherCost) {
+            Random random = new Random();
+            int costDiff = random.nextInt(otherCost / 50);
+            boolean plus = random.nextBoolean();
+            if(plus) {
+                cost = otherCost + costDiff;
+            } else {
+                cost = otherCost - costDiff;
+            }
+        }
+
+
+        public void setCostForFollowerArtist(int otherCost) {
+            Random random = new Random();
+            int costDiff = random.nextInt(otherCost / 50);
+            boolean plus = random.nextBoolean();
+            int multiplier = 5;
+            if(plus) {
+                cost = otherCost / multiplier + costDiff;
+            } else {
+                cost = otherCost / multiplier - costDiff;
+            }
         }
     }
 
